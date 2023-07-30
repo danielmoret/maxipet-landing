a8e1944ce130c745e44164778538a6a7b63c3c02
danielmoret <dmoret17@gmail.com>
2023-07-30 17:07:48 -0400

    feat: add services component and service card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Banner from '../components/Banner';
 import Categories from '../components/Categories';
 import Products from '../components/Products';
 import Ads from '../components/Ads';
+import Services from '../components/Services';
 import cat from '../public/images/products/cat.png';
 import ad1 from '../public/images/ads/ad1.png';
 import ad2 from '../public/images/ads/ad2.png';
@@ -24,6 +25,7 @@ function HomePage() {
       <Products title="productos populares" reverse={false} />
       <Ads img={ad2} alt="perro jugando" />
       <Products title="últimos productos" reverse img={cat.src} alt="gato" />
+      <Services />
     </div>
   );
 }
